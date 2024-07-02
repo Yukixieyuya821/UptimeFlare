408a1827c37caa7f6ae18062234549dde65b7c11
Yukixieyuya821 <53206696+Yukixieyuya821@users.noreply.github.com>
2024-07-02 15:10:48 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -3,7 +3,7 @@ const pageConfig = {
   title: "Yuki's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/Yukixieyuya821', label: 'GitHub' },
+    // { link: 'https://github.com/Yukixieyuya821', label: 'GitHub' },
     { link: 'https://chat.yukixieyu.cn', label: 'Yuki ChatGPT' }
   ],
 }
@@ -14,42 +14,42 @@ const workerConfig = {
   // Define all your monitors here
   monitors: [
     // Example HTTP Monitor
-    {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'github',
-      // `name` is used at status page and callback message
-      name: 'My Github',
-      // `method` should be a valid HTTP Method
-      method: 'GET',
-      // `target` is a valid URL
-      target: 'https://github.com/Yukixieyuya821',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: 'This is a tooltip for my github',
-      // // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      // statusPageLink: 'https://github.com/Yukixieyuya821',
-      // // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      // expectedCodes: [200],
-      // // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      // timeout: 10000,
-      // // [OPTIONAL] headers to be sent
-      // headers: {
-      //   'User-Agent': 'Uptimeflare',
-      //   Authorization: 'Bearer YOUR_TOKEN_HERE',
-      // },
-      // // [OPTIONAL] body to be sent
-      // body: 'Hello, world!',
-      // // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      // responseKeyword: 'success',
-      // // [OPTIONAL] if specified, the check will run in your specified region,
-      // // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      // checkLocationWorkerRoute: 'https://xxx.example.com',
-    },
+    // {
+    //   // `id` should be unique, history will be kept if the `id` remains constant
+    //   id: 'github',
+    //   // `name` is used at status page and callback message
+    //   name: 'My Github',
+    //   // `method` should be a valid HTTP Method
+    //   method: 'GET',
+    //   // `target` is a valid URL
+    //   target: 'https://github.com/Yukixieyuya821',
+    //   // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
+    //   tooltip: 'This is a tooltip for my github',
+    //   // // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
+    //   // statusPageLink: 'https://github.com/Yukixieyuya821',
+    //   // // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
+    //   // expectedCodes: [200],
+    //   // // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
+    //   // timeout: 10000,
+    //   // // [OPTIONAL] headers to be sent
+    //   // headers: {
+    //   //   'User-Agent': 'Uptimeflare',
+    //   //   Authorization: 'Bearer YOUR_TOKEN_HERE',
+    //   // },
+    //   // // [OPTIONAL] body to be sent
+    //   // body: 'Hello, world!',
+    //   // // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
+    //   // responseKeyword: 'success',
+    //   // // [OPTIONAL] if specified, the check will run in your specified region,
+    //   // // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
+    //   // checkLocationWorkerRoute: 'https://xxx.example.com',
+    // },
     {
       id: 'yuki_chatgpt',
       name: 'Yuki ChatGPT',
       method: 'GET',
       target: 'https://chat.yukixieyu.cn',
-      tooltip: 'GPT'
+      tooltip: 'An intelligent AI chatbot for seamless, human-like conversations and information retrieval.(一个智能的 AI 聊天机器人，可以进行无缝的、类似人类的对话和信息检索。)'
     }
     // // Example TCP Monitor
     // {
